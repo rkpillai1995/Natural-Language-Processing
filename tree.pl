f910ee62b81ee3265c37d92b76f7a72326d4e5c5
Rajkumar <59179138+rkpillai1995@users.noreply.github.com>
2020-11-05 14:08:33 -0500

    Add files via upload

---
+++ tree.pl
@@ -0,0 +1,270 @@
+parent(richardFranklin,carolRomanowski).
+parent(margaretMoore,carolRomanowski).
+parent(oscarFranklin,ellenFranklin).
+parent(blancheAdams,ellenFranklin).
+parent(oscarFranklin,haroldFranklin).
+parent(blancheAdams,haroldFranklin).
+parent(oscarFranklin,dorisFranklin).
+parent(blancheAdams,dorisFranklin).
+parent(oscarFranklin,jeanFranklin).
+parent(blancheAdams,jeanFranklin).
+parent(oscarFranklin,llewellynoscarFranklin).
+parent(blancheAdams,llewellynoscarFranklin).
+parent(oscarFranklin,richardFranklin).
+parent(blancheAdams,richardFranklin).
+parent(llewellynFranklin,oscarFranklin).
+parent(katherineTwining,oscarFranklin).
+parent(miltonAdams,blancheAdams).
+parent(miltonAdams,edithAdams).
+parent(miltonAdams,ednaAdams).
+parent(miltonAdams,ireneAdams).
+parent(miltonAdams,dorothyAdams).
+parent(miltonAdams,velmaAdams).
+parent(miltonAdams,sadieAdams).
+parent(ellenWhite,blancheAdams).
+parent(ellenWhite,edithAdams).
+parent(ellenWhite,ednaAdams).
+parent(ellenWhite,ireneAdams).
+parent(ellenWhite,dorothyAdams).
+parent(ellenWhite,velmaAdams).
+parent(ellenWhite,sadieAdams).
+parent(oscaraFranklin,llewellynFranklin).
+parent(maryDoak,llewellynFranklin).
+parent(oscaraFranklin,williamFranklin).
+parent(maryDoak,williamFranklin).
+parent(oscaraFranklin,edwinFranklin).
+parent(maryDoak,edwinFranklin).
+parent(oscaraFranklin,delvinFranklin).
+parent(maryDoak,delvinFranklin).
+parent(oscaraFranklin,charlesFranklin).
+parent(maryDoak,charlesFranklin).
+parent(jesseTwining,katherineTwining).
+parent(maryGoodwin,katherineTwining).
+parent(averyWhite,ellenWhite).
+parent(dencyPhillips,ellenWhite).
+parent(averyWhite,violaWhite).
+parent(dencyPhillips,violaWhite).
+parent(averyWhite,doraWhite).
+parent(dencyPhillips,doraWhite).
+parent(averyWhite,bessieWhite).
+parent(dencyPhillips,bessieWhite).
+parent(doraWhite,alfredWhiteson). 
+parent(williamWrightson, alfredWhiteson).
+parent(eliWhite,averyWhite).
+parent(lydiaJohnson,averyWhite).
+parent(joshuaWhite,eliWhite).
+parent(elizabethMorse,eliWhite).
+parent(sarahMead,joshuaWhite).
+parent(davidWhite,joshuaWhite).
+parent(mercyHazen,sarahMead).
+parent(abnerMead,sarahMead).
+parent(calebHazen,mercyHazen).
+parent(mercyBradstreet,mercyHazen).
+parent(johnBradstreet,mercyBradstreet).
+parent(anneDudley,johnBradstreet).
+parent(gov_simonBradstreet,johnBradstreet).
+parent(sir_thomasDudley,anneDudley).
+parent(dorothyYorke,anneDudley).
+parent(sir_rogerDudley,sir_thomasDudley).
+parent(susannahThorne,sir_thomasDudley).
+parent(sir_henryDudley,sir_rogerDudley).
+parent(unknownWifeOfsir_henryDudley,sir_rogerDudley).
+parent(sir_johnSutton,sir_henryDudley).
+parent(cicelyGray,sir_henryDudley).
+parent(sir_edwardSutton, sir_johnSutton).
+parent(ceciliaWilloughby,sir_johnSutton).
+parent(sir_williamWilloughby, ceciliaWilloughby).
+parent(janeStrangeways,ceciliaWilloughby).
+parent(thomasStrangeways, janeStrangeways).
+parent(catherineNeville,janeStrangeways).
+parent(richardPlantagenet, king_richardIII).
+parent(cecilyNeville,king_richardIII).
+parent(ralphdeNeville,catherineNeville).
+parent(joanBeaufort,catherineNeville).
+parent(ralphdeNeville, cecilyNeville).
+parent(joanBeaufort,cecilyNeville).
+parent(amosAdams, miltonAdams).
+parent(betsyGriggs, miltonAdams).
+parent(leviAdams, amosAdams).
+parent(hannahHeald, amosAdams).
+parent(johnAdams2, leviAdams).
+parent(joannaMunroe, leviAdams).
+parent(thomasAdams, johnAdams2).
+parent(josephAdams, thomasAdams).
+parent(johnAdams1, josephAdams).
+parent(henryAdams, johnAdams1).
+parent(annaHowe, josephAdams).
+parent(lydiaChadwick, johnAdams).
+parent(rebeccaCutter, thomasAdams).
+parent(ltJonasMunroe,joannaMunroe).
+parent(johnMunroe, ltJonasMunroe).
+parent(williamMunroe, johnMunroe).
+parent(hoseaPhillips2,dencyPhillips).
+parent(hoseaPhillips1, hoseaPhillips2).
+parent(johnPhillips, hoseaPhillips1).
+parent(dimmisAylesworth, johnPhillips).
+parent(maryFranklin,dimmisAylesworth).
+parent(jamesFranklin1, maryFranklin).
+parent(jamesFranklin1, jamesFranklin2).
+parent(jamesFranklin2, elishaFranklin).
+parent(elishaFranklin, johnFranklin).
+parent(johnFranklin, anthonyFranklin).
+parent(anthonyFranklin, esekFranklin).
+parent(esekFranklin, oscaraFranklin).
+parent(arthurAylesworth, dimmisAylesworth).
+
+
+
+female(carolRomanowski).
+female(margaretMoore).
+female(ellenFranklin).
+female(dorisFranklin).
+female(jeanFranklin).
+female(blancheAdams).
+female(edithAdams).
+female(ednaAdams).
+female(ireneAdams).
+female(dorothyAdams).
+female(velmaAdams).
+female(sadieAdams).
+female(ellenWhite).
+female(lydiaJohnson).
+female(elizabethMorse).
+female(katherineTwining).
+female(maryDoak).
+female(maryGoodwin).
+female(dencyPhillips).
+female(sarahMead).
+female(mercyHazen).
+female(mercyBradstreet).
+female(anneDudley).
+female(dorothyYorke).
+female(susannahThorne).
+female(unknownWifeOfsir_henryDudley).
+female(cicelyGray).
+female(ceciliaWilloughby).
+female(janeStrangeways).
+female(catherineNeville).
+female(cecilyNeville).
+female(joanBeaufort).
+female(joannaMunroe).
+female(dimmisAylesworth).
+female(betsyGriggs).
+female(hannahHeald).
+female(rebeccaCutter).
+female(annaHowe).
+female(maryFranklin).
+female(violaWhite).
+female(doraWhite).
+female(bessiewhite).
+male(richardFranklin).
+male(haroldFranklin).
+male(llewellynoscarFranklin).
+male(oscarFranklin).
+male(llewellynFranklin).
+male(charlesFranklin).
+male(delvinFranklin).
+male(edwinFranklin).
+male(williamFranklin).
+male(oscaraFranklin).
+male(miltonAdams).
+male(jesseTwining).
+male(averyWhite).
+male(eliWhite).
+male(calebHazen).
+male(abnerMead).
+male(joshuaWhite).
+male(davidWhite).
+male(johnBradstreet).
+male(gov_simonBradstreet).
+male(sir_thomasDudley).
+male(sir_rogerDudley).
+male(sir_henryDudley).
+male(sir_johnSutton).
+male(sir_williamWilloughby).
+male(thomasStrangeways).
+male(richardPlantagenet).
+male(king_richardIII).
+male(ralphdeNeville).
+male(amosAdams).
+male(leviAdams).
+male(johnAdams2).
+male(johnAdams1).
+male(thomasAdams).
+male(ltJonasMunroe).
+male(johnMunroe).
+male(williamMunroe).
+male(hoseaPhillips1).
+male(hoseaPhillips2).
+male(josephAdams).
+male(henryAdams).
+male(jamesFranklin1).
+male(jamesFranklin2).
+male(johnFranklin).
+male(elishaFranklin).
+male(anthonyFranklin).
+male(esekFranklin). 
+male(arthurAylesworth).
+male(alfredWhiteson).
+male(williamWrightson).
+
+
+%!  %%%%%%%%%% offspring
+offspring(A,B) :- parent(B,A).
+
+
+%!  %%%%%%%%%% grandparents
+grandparents(C,A):-
+    parent(C,B), parent(B,A).
+
+
+%!  %%%%%%%%%% ancestors
+ancestor(A,C):-
+    offspring(C,A);(offspring(B,A),ancestor(B,C)).
+
+
+%!  %%%%%%%%%%%%%%% sibling
+sibling(A,B):-
+   offspring(A,C) ,offspring(B,C) ,A \= B.
+
+
+%!  %%%%%%%%%% uncle
+uncle(A,B) :-
+		 offspring(B,C), sibling(C,D), male(A),uncle(A,D).
+
+uncle(A,B) :-
+		 ancestor(D,B), sibling(D,A), male(A).
+
+
+%!  %%%%%%%%%% aunt
+aunt(A,B) :-
+		 offspring(B,C), sibling(C,D), female(A),aunt(A,D).
+
+aunt(A,B) :-
+		 ancestor(D,B), sibling(D,A), female(A).
+
+		 
+%!  %%%%%%%%%%%%%% cousin
+cousin(A,B) :-
+    ancestor(C,A) , ancestor(D,B) , C\=D, sibling(C,D).
+
+cousin(A,B):- 
+	offspring(A,C), offspring(B,D), C\=D, cousin(C,D).
+
+	
+	
+
+%!  %%%%%%%%%%%%%% ancestor_pair		 
+ancestorpair(A,B) :- 
+				setof(A, offspring(B,A), A);(offspring(B,C), ancestorpair(A,C)).
+				
+				
+				
+%!  %%%%%%%%%%%%%%%%%%%%%%% son
+son(A,B):-male(A),offspring(A,B).
+		 		
+		 
+
+
+
+   
